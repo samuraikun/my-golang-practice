95d700753034c53937313e3b3b289801daf66b4b
samuraikun <tazitaziawawa@gmail.com>
2020-01-12 14:06:31 +0900

    add multiplexer

--- 03_tcp_server/tcp_for_http/main.go
+++ 03_tcp_server/tcp_for_http/main.go
@@ -19,6 +19,7 @@ func main() {
 		conn, err := li.Accept()
 		if err != nil {
 			log.Fatalln(err.Error())
+			continue
 		}
 		go handle(conn)
 	}
@@ -28,7 +29,6 @@ func handle(conn net.Conn) {
 	defer conn.Close()
 
 	request(conn)
-	respond(conn)
 }
 
 func request(conn net.Conn) {
@@ -38,11 +38,7 @@ func request(conn net.Conn) {
 		ln := scanner.Text()
 		fmt.Println(ln)
 		if i == 0 {
-			// request line
-			m := strings.Fields(ln)[0] // method
-			u := strings.Fields(ln)[1] // uri
-			fmt.Println("***METHOD", m)
-			fmt.Println("***URI", u)
+			mux(conn, ln)
 		}
 		if ln == "" {
 			// headers are done
@@ -52,9 +48,108 @@ func request(conn net.Conn) {
 	}
 }
 
-func respond(conn net.Conn) {
+func mux(conn net.Conn, ln string) {
+	// request line
+	m := strings.Fields(ln)[0] // method
+	u := strings.Fields(ln)[1] // uri
+	fmt.Println("***METHOD", m)
+	fmt.Println("***URI", u)
 
-	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Hello World</strong></body></html>`
+	// multiplexer
+	if m == "GET" && u == "/" {
+		index(conn)
+	}
+	if m == "GET" && u == "/about" {
+		about(conn)
+	}
+	if m == "GET" && u == "/contact" {
+		contact(conn)
+	}
+	if m == "GET" && u == "/apply" {
+		apply(conn)
+	}
+	if m == "POST" && u == "/apply" {
+		applyProcess(conn)
+	}
+}
+
+func index(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>INDEX</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func about(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>ABOUT</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func contact(conn net.Conn) {
+
+	body := `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>CONTACT</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func apply(conn net.Conn) {
+
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body>
+	<strong>APPLY</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	<form method="POST" action="/apply">
+	<input type="submit" value="apply">
+	</form>
+	</body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func applyProcess(conn net.Conn) {
+
+	body := `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>APPLY PROCESS</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>`
 
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
